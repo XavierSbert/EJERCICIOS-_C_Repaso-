ea0272d378bda968ef1ca1e26ee06aca81a9497a
XavierSbert <xaviersbert@paucasesnovescifp.cat>
2020-10-23 18:46:41 +0200

    Act 2 DataTime

---
+++ ReviewExercises/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReviewExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list1 = new List<string>()
+            {
+            "John", "Mary", "Sue", "Greg", "Yolanda", "Jose", "Bill"
+            };
+
+            foreach (var list in list1)
+                Console.WriteLine("Hello " + list);
+
+            Console.WriteLine("Press ENTER to continue...");
+            Console.ReadLine();
+
+            List<PersonModel> peopleModels = GetPeopleModels();
+
+            foreach (PersonModel person in peopleModels)
+            {
+                Console.WriteLine($"Hello {person.FirstName} {person.LastName}" );
+            }
+
+            Console.ReadLine();
+            
+        }
+
+        private static List<PersonModel> GetPeopleModels()
+        {
+            List<PersonModel> output = new List<PersonModel>();
+
+            output.Add(new PersonModel { FirstName = "Tim", LastName = "Corey" }); 
+            output.Add(new PersonModel { FirstName = "Bill", LastName = "McCoy" }); 
+            output.Add(new PersonModel { FirstName = "Mary", LastName = "Jones" }); 
+            output.Add(new PersonModel { FirstName = "Sue", LastName = "Smith" }); 
+       
+       
+       
+       
+       
+
+            return output;
+        }
+    }
+}
+    
---
+++ ReviewExercises_datetime/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReviewExercises_datetime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Give me a date: ");
+            string intro = Console.ReadLine();
+            DateTime dateTime = DateTime.ParseExact(intro, "dd/MM/yyyy", null);
+            var now = DateTime.Now;
+            var day = (now - dateTime).ToString(@"dd");
+
+            if (dateTime < now)
+            {
+                Console.WriteLine("It has been " + day + " days since " + dateTime.ToString("dd-MM-yyyy"));
+            }
+            else
+            {
+                Console.WriteLine(day + " days left for " + dateTime.ToString("dd-MM-yyyy"));
+            }
+
+            Console.Write("Give me a time: ");
+            string intro2 = Console.ReadLine();
+
+            DateTime dateTime2 = DateTime.ParseExact(intro2, "h:mm tt", CultureInfo.InvariantCulture);
+
+            TimeSpan res = DateTime.Now.Subtract(dateTime2);
+
+            Console.WriteLine($"{ intro2 } was {res.Hours} hours and {res.Minutes} minutes");
+
+            Console.ReadLine();
+
+        }
+    }
+}
